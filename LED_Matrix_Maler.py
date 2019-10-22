6865da4cc7fa3bbf320f942fd3ff0e4b6265bb0d
20David04 <david.max.luebke@gmail.com>
2019-10-22 11:41:17 +0200

    LED_Matrix_Maler fitted to Hardware deleted unnessecery stuff

--- LED_Matrix_Maler.py
+++ LED_Matrix_Maler.py
@@ -20,31 +20,8 @@ def demo(n, block_orientation, rotate, inreverse):
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation,
                      rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
-    print("Created device")
 
-
-    msg = "Slow scrolling: The quick brown fox jumps over the lazy dog"
-    print(msg)
-    show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0.1)
-
-    print("Vertical scrolling")
-    words = [
-        "Victor", "Echo", "Romeo", "Tango", "India", "Charlie", "Alpha",
-        "Lima", " ", "Sierra", "Charlie", "Romeo", "Oscar", "Lima", "Lima",
-        "India", "November", "Golf", " "
-    ]
-
-    virtual = viewport(device, width=device.width, height=len(words) * 8)
-    with canvas(virtual) as draw:
-        for i, word in enumerate(words):
-            text(draw, (0, i * 8), word, fill="white", font=proportional(CP437_FONT))
-
-    for i in range(virtual.height - device.height):
-        virtual.set_position((0, i))
-        time.sleep(0.05)
-
-    msg = "Brightness"
-    print(msg)
+    msg = "Hallo"
     show_message(device, msg, fill="white")
 
     time.sleep(1)
