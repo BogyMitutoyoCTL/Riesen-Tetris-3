7ccf8608ec761845e64f962ffd566646f27b9e80
20David04 <david.max.luebke@gmail.com>
2019-10-22 09:53:08 +0200

    L-Tetrisblock+drehung

--- Blöcke.py
+++ Blöcke.py
@@ -1,25 +1,42 @@
 class Testrisblock:
     def __init__(self, alphalist):
-        self.color=(100,0,20)
-        self.posotion=(3,5)
-        self.orientations=alphalist
-        self.orientation=0
+        self.color = (100, 0, 20)
+        self.posotion = (3, 5)
+        self.orientations = alphalist
+        self.orientation = 0
 
     def showme(self):
-        templist=self.orientations[0]
+        templist = self.orientations[self.orientation]
         for row in templist:
             for colum in row:
-                if colum ==1:
-                    print("x",end="")
+                if colum == 1:
+                    print("x", end="")
                 else:
-                    print(" ",end="")
+                    print(" ", end="")
             print()
+        print()
+    def turnright(self):
+        self.orientation +=1
+        if self.orientation == 4:
+            self.orientation = 0
+    def turnleft(self):
+        self.orientation -=1
+        if self.orientation == 0:
+            self.orientation = 3
 
-list1=[[1,1,0,],[0,1,0,],[0,1,0]]
-list2=[[0,0,0],[1,1,1],[1,0,0]]
-list3=[[0,1,0],[0,1,0],[0,1,1]]
-list4=[[1,0,0],[1,1,1],[0,0,0]]
-alphalist=[list1,list2,list3,list4]
-L=Testrisblock(alphalist)
+
+
+list1 = [[1, 1, 0, ], [0, 1, 0, ], [0, 1, 0]]
+list2 = [[0, 0, 0], [1, 1, 1], [1, 0, 0]]
+list3 = [[0, 1, 0], [0, 1, 0], [0, 1, 1]]
+list4 = [[0, 0, 1], [1, 1, 1], [0, 0, 0]]
+alphalist = [list1, list2, list3, list4]
+L = Testrisblock(alphalist)
 L.showme()
-
+L.turnright()
+L.showme()
+L.turnright()
+L.showme()
+for Test in range(0,11):
+    L.turnright()
+    L.showme()
