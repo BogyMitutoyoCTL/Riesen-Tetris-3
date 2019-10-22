47dfa11f4247905290d28ec6e3862b3c6d304a21
20David04 <david.max.luebke@gmail.com>
2019-10-22 11:38:31 +0200

    LED_Matrix_Maler fitted to Hardware deleted unnessecery stuff

--- LED_Matrix_Maler.py
+++ LED_Matrix_Maler.py
@@ -22,22 +22,6 @@ def demo(n, block_orientation, rotate, inreverse):
                      rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
     print("Created device")
 
-    # start demo
-    msg = "MAX7219 LED Matrix Demo"
-    print(msg)
-    show_message(device, msg, fill="white", font=proportional(CP437_FONT))
-    time.sleep(1)
-
-    msg = "Fast scrolling: Lorem ipsum dolor sit amet, consectetur adipiscing\
-    elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut\
-    enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut\
-    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in\
-    voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint\
-    occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit\
-    anim id est laborum."
-    msg = re.sub(" +", " ", msg)
-    print(msg)
-    show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0)
 
     msg = "Slow scrolling: The quick brown fox jumps over the lazy dog"
     print(msg)
