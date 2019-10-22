e1c659a603c4973bbb892d0bda72bb8db102e460
20David04 <david.max.luebke@gmail.com>
2019-10-22 10:44:21 +0200

    Start counting at 0

--- RandomBlock.py
+++ RandomBlock.py
@@ -3,8 +3,8 @@ from random import randint
 
 class RandomBlock:
     def __init__(self):
-        self.minimum = 1
-        self.maximum = 7
+        self.minimum = 0
+        self.maximum = 6
 
     def get_random_block(self):
         return randint(self.minimum, self.maximum)
