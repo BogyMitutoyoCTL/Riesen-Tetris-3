73a73d7a354fafa141badcd3ec0a67fd512d69ec
Johannes <jhnnsgruber@web.de>
2019-10-24 14:24:30 +0200

    block starts at right position

--- Game_Engine.py
+++ Game_Engine.py
@@ -10,122 +10,86 @@ import Points
 import Tetrisblock
 import Control_feedback
 
-# import Sound
+import Sound
+
+
 # import control_feedback
+
+def before_game(playground, painter, controller):
+    s = Startmenu.Startmenu(playground)
+    painter.paint(playground)
+    while True:
+        buttons = controller.pressed()
+        if "Start" in buttons:
+            playground.clear()
+            painter.paint(playground)
+            break
+
+
+def play_game(playground, clock, painter, leonardo, controller, sound):
+    objekt = RandomBlock.RandomBlock()
+    currentBlock = objekt.get_random_block()
+    nextBlock = objekt.get_random_block()
+    points = Points.Points()
+    playground.put_block(currentBlock)
+    while True:
+
+        leonardo.draw(str(points.points), nextBlock)
+
+        x, y = currentBlock.position
+
+        for _ in range(4):
+            buttons = controller.pressed()
+
+            if "right" in buttons:
+                x = x + 1
+                print("right")
+
+            if "left" in buttons:
+                x = x - 1
+                print("left")
+
+            if "down" in buttons:
+                y = y + 1
+                print("down")
+            if "B" in buttons:
+                currentBlock.turnright()
+
+            if "Y" in buttons:
+                currentBlock.turnleft()
+
+            playground.put_block(currentBlock)
+            painter.paint(playground)
+            playground.remove_block(currentBlock)
+            clock.tick(4)
+            c = playground.fullrow()
+            if len(c) > 0:
+                playground.delete_line(c)
+                points.lines(len(c))
+                if len(c) == 1:
+                    sound.complete_line1()
+                if len(c) == 2:
+                    sound.complete_line2()
+                if len(c) == 3:
+                    sound.complete_line3()
+                if len(c) == 4:
+                    sound.complete_line4()
+
+        if y == 15:
+            currentBlock = nextBlock
+            nextBlock = objekt.get_random_block()
+
+        else:
+            currentBlock.position = x, y + 1
+
+
 pygame.init()
 clock = pygame.time.Clock()
 leonardo = LED_Matrix_Maler.Painter()
 tetrisblock = Tetrisblock.Tetrisblock
 playground = Playground.Playground(10, 20)
-
-objekt = RandomBlock.RandomBlock()
 controller = Control_feedback.Controller()
-currentBlock = objekt.get_random_block()
-nextBlock = objekt.get_random_block()
-
-playground.put_block(currentBlock)
-
 painter = Ws2812Painter.Ws2812Painter()
-s = Startmenu.Startmenu(playground)
-
-painter.paint(playground)
-time.sleep(3)
-playground.clear()
-playground = Playground.Playground(10, 20)
-painter.paint(playground)
-# Wenn Start gedrückt wird muss ein Random Block von oben kommen
-while True:
-    # Ein Random Block wird in der Led-Matrix gemalt
-    # Der Random Block kommt oben aufs Spielfeld
-    # Ein neuer Random Block wird auf der Led-Matrix gezeichnet
-    # Der Block fällt in einer Zeitspanne immer eine Reihe nach unten
-    # Der Block fällt in einer Zeitspanne immer eine Reihe nach unten
-    # ...
-    # Der Block kommt unten an oder trifft auf einen anderen Block evtl. mit Sound
-    # Wenn eine Reihe voll ist werden Punkte hinzugezählt
-    # Das Spiel wird schneller, da es mehr Punkte gibt
-    # Der Block von der Led-Matrix kommt aufs Spielfeld
-    # kann er nicht mehr aufs Spielfeld, endet das Spiel. Wenn nicht, geht das Spiel weiter
-    # Alles wiederholt sich
-    leonardo.draw(str(points.points), nextBlock)
-
-    x, y = currentBlock.position
-
-    for _ in range(4):
-        buttons = controller.pressed()
-
-        if "right" in buttons:
-            x = x + 1
-            print("right")
-
-        if "left" in buttons:
-            x = x - 1
-            print("left")
-
-        if "down" in buttons:
-            y = y + 1
-            print("down")
-        if "B" in buttons:
-            currentBlock.turnright()
-
-        if "Y" in buttons:
-            currentBlock.turnleft()
-        if "A" in buttons:
-            #playground.coordinate_system[19][0] = (255, 0, 0)
-            #playground.coordinate_system[19][1] = (255, 0, 0)
-            #playground.coordinate_system[19][2] = (255, 0, 0)
-            #playground.coordinate_system[19][3] = (255, 0, 0)
-            #playground.coordinate_system[19][4] = (255, 0, 0)
-            #playground.coordinate_system[19][5] = (255, 0, 0)
-            #playground.coordinate_system[19][6] = (255, 0, 0)
-            #playground.coordinate_system[19][7] = (255, 0, 0)
-            #playground.coordinate_system[19][8] = (255, 0, 0)
-            #playground.coordinate_system[19][9] = (255, 0, 0)
-            #playground.coordinate_system[18][0] = (255, 0, 0)
-            #playground.coordinate_system[18][1] = (255, 0, 0)
-            #playground.coordinate_system[18][2] = (255, 0, 0)
-            #playground.coordinate_system[18][3] = (255, 0, 0)
-            #playground.coordinate_system[18][4] = (255, 0, 0)
-            #playground.coordinate_system[18][5] = (255, 0, 0)
-            #playground.coordinate_system[18][6] = (255, 0, 0)
-            #playground.coordinate_system[18][7] = (255, 0, 0)
-            #playground.coordinate_system[18][8] = (255, 0, 0)
-            #playground.coordinate_system[18][9] = (255, 0, 0)
-            #playground.coordinate_system[17][0] = (255, 0, 0)
-            #playground.coordinate_system[17][1] = (255, 0, 0)
-            #playground.coordinate_system[17][2] = (255, 0, 0)
-            #playground.coordinate_system[17][3] = (255, 0, 0)
-            #playground.coordinate_system[17][4] = (255, 0, 0)
-            #playground.coordinate_system[17][5] = (255, 0, 0)
-            #playground.coordinate_system[17][6] = (255, 0, 0)
-            #playground.coordinate_system[17][7] = (255, 0, 0)
-            #playground.coordinate_system[17][8] = (255, 0, 0)
-            #playground.coordinate_system[17][9] = (255, 0, 0)
-            #playground.coordinate_system[16][0] = (255, 0, 0)
-            #playground.coordinate_system[16][1] = (255, 0, 0)
-            #playground.coordinate_system[16][2] = (255, 0, 0)
-            #playground.coordinate_system[16][3] = (255, 0, 0)
-            #playground.coordinate_system[16][4] = (255, 0, 0)
-            #playground.coordinate_system[16][5] = (255, 0, 0)
-            #playground.coordinate_system[16][6] = (255, 0, 0)
-            #playground.coordinate_system[16][7] = (255, 0, 0)
-            #playground.coordinate_system[16][8] = (255, 0, 0)
-            #playground.coordinate_system[16][9] = (255, 0, 0)
-            pass
-
-        playground.put_block(currentBlock)
-        painter.paint(playground)
-        playground.remove_block(currentBlock)
-        clock.tick(8)
-        c = playground.fullrow()
-        if len(c) > 0:
-            playground.delete_line(c)
-            points.lines(len(c))
-        # leonardo.write_text(str(points.points))
-
-    if y == 15:
-        currentBlock = nextBlock
-        nextBlock = objekt.get_random_block()
-
-    else:
-        currentBlock.position = x, y + 1
+sound = Sound.Sound()
+before_game(playground, painter, controller)
+play_game(playground, clock, painter, leonardo, controller, sound)
--- RandomBlock.py
+++ RandomBlock.py
@@ -10,5 +10,7 @@ class RandomBlock:
     def get_random_block(self):
         R = randint(self.minimum, self.maximum)
         RndBlock = Tetrisblock.Blocklist[R]
-        RndBlock.position = 0,0
+        field=RndBlock.orientations[0]
+        lines=len(field)
+        RndBlock.position = 4, lines*-1
         return RndBlock
